f0a5d8fb907ba45bacff5994fe2d6079ce672a07
kumar gayatri <khoomkummi@gmail.com>
2016-09-10 23:54:28 -0500

    added . to image

--- New_Image_tagging/app/resume/resumeconfig.js
+++ New_Image_tagging/app/resume/resumeconfig.js
@@ -3,7 +3,7 @@ var resumeconfig =
   "basics": {
     "name": "Kumar Jayaram Gayatri",
     "label": "Web Developer, Data Scientist (Researcher)",
-    "picture": "/New_Image_tagging/image/profilePhoto.jpg",
+    "picture": "./New_Image_tagging/image/profilePhoto.jpg",
     "email": "kumar_gokarn@yahoo.co.in",
     "phone": "+1 6823670300",
     "website": "http://kumargayu.github.io/Projects/",
