59857567e9cc018938737098136f144a921e93ff
kumar gayatri <khoomkummi@gmail.com>
2016-11-12 20:40:02 -0600

    corrected

--- Web_Projects/app/App.js
+++ Web_Projects/app/App.js
@@ -8,8 +8,6 @@ var IndexRoute = require('react-router').IndexRoute;
 var Mainapp = require('./Mainapp');
 var Main = require('./components/Main');
 var Resume = require('./resume/Resume');
-var Llamaprofile = require('./Llama/llamaprofile');
-var Reasons = require('./reasons/reasons');
 
 var useRouterHistory = require('react-router').useRouterHistory;
 var createHashHistory = require('history').createHistory;
@@ -20,10 +18,7 @@ var App = React.createClass({
       return (
         <Router useKey={false} history={appHistory}>
           <Route path="/Projects/" component={Mainapp}>
-            <IndexRoute component={Llamaprofile} />
-            <Route path="about" component={Resume} />
-            <Route path="home" component={Llamaprofile} />
-            <Route path="reasons" component={Reasons} />
+            <IndexRoute component={Resume} />
             <Route path="projects" component={Main} />
           </Route>
         </Router>
